42d528835a52ecb34ac7964fbd181213355cc494
Jinesh Devasia <Jinesh.Devasia@rsa.com>
2015-11-24 11:59:42 +0530

    Support for git pull in python

---
+++ python/git_api/git_pull.py
@@ -0,0 +1,18 @@
+from git.api import Pull
+
+
+class GitPull:
+    """An Github API for Pull"""
+    
+    def __init__()
+        """Initilaze the git repo with URL and authentication"""
+        
+        pass
+        
+    def pull(*args)
+        """Pull the git repo from the branch
+        
+        Args:
+            *args: Optional arguments for pull
+        """
+    
